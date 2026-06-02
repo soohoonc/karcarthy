16214b1a6c97afe2a16e0c9cdd0e129ef8f69388
SooHoon Choi <soohoonchoi@gmail.com>
2026-06-01 18:05:54 -0700

    organize docs navigation

--- docs-site/src/app/layout.tsx
+++ docs-site/src/app/layout.tsx
@@ -1,16 +1,10 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 
-const geistSans = Geist({
+const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-geist-sans',
-});
-
-const geistMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-geist-mono',
 });
 
 function metadataBase() {
@@ -33,11 +27,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable}`}
-      suppressHydrationWarning
-    >
+    <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
